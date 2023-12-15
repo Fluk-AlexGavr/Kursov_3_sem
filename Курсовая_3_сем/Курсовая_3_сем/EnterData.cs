9db4570c24863adc579a4b2adaf4ebb4893d8fcb
Fluk <aleshagavr@mail.ru>
2023-12-15 19:26:41 +0300

    исправлены ошибки

--- Курсовая_3_сем/Курсовая_3_сем/EnterData.cs
+++ Курсовая_3_сем/Курсовая_3_сем/EnterData.cs
@@ -8,7 +8,7 @@ namespace Курсовая_3_сем
 {
     public static class EnterData
     {
-        public static string EntData()
+        private static string EntData()
         {
             string str = Console.ReadLine();
             return str; 
@@ -18,34 +18,5 @@ namespace Курсовая_3_сем
         {
             return EntData();
         }
-        public static int IntData(string str)
-        {
-            int i = 0;
-            try
-            {
-                i = Convert.ToInt32(str);
-                return i;
-            }
-            catch
-            {
-                ExceptionPrinter.Print("Ожидалось число");
-                return i;
-            }
-        }
-        public static double DoubleData(string str)
-        {
-            double i = 0;
-            try
-            {
-                i = Convert.ToDouble(str);
-                return i;
-            }
-            catch
-            {
-                ExceptionPrinter.Print("Ожидалось число");
-                return i;
-            }
-        }
-
     }
 }
